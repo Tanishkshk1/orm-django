63a9da2ca7b38017f9f770b831f3a3b690e914d0
tanishkshk1 <2821108.cse@pietgroup.co.in@gmail.com>
2024-08-06 11:55:11 +0530

    implemnted caching

--- myapp/cache_utils.py
+++ myapp/cache_utils.py
@@ -1,6 +1,6 @@
 import hashlib
 from django.core.cache import cache
-
+'''
 def generate_cache_key(view_name, *args, **kwargs):
     key = f"{view_name}:{':'.join(map(str, args))}:{':'.join(f'{k}={v}' for k, v in kwargs.items())}"
     return hashlib.md5(key.encode('utf-8')).hexdigest()
@@ -10,3 +10,22 @@ def get_cache_data(cache_key):
 
 def set_cache_data(cache_key, data, timeout=60*15):
     cache.set(cache_key, data, timeout)
+
+'''
+
+def generate_cache_key(prefix, *args, **kwargs):
+    key = prefix
+    for arg in args:
+        key += f'_{arg}'
+    for k, v in kwargs.items():
+        key += f'_{k}_{v}'
+    return key
+
+def get_cache_data(cache_key):
+    return cache.get(cache_key)
+
+def set_cache_data(cache_key, data, timeout=3600):
+    cache.set(cache_key, data, timeout)
+
+def delete_cache_key(cache_key):
+    cache.delete(cache_key)
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets
+from django.core.cache import cache
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render,redirect
 from rest_framework.response import Response
 from django.http import HttpResponseRedirect
 from django.views.generic import View
 from .forms import EmployeeForm ,DepartmentForm
-from .cache_utils import generate_cache_key, get_cache_data, set_cache_data
+from .cache_utils import generate_cache_key, get_cache_data, delete_cache_key, set_cache_data
 from .models import Department, Employee, DeptEmp, DeptManager, Salary, Title
 from .serializers import DepartmentSerializer, EmployeeSerializer, DeptEmpSerializer, DeptManagerSerializer, SalarySerializer, TitleSerializer
 from myapp.permissions import (CanViewDepartment, CanEditDepartment, CanDeleteDepartment,
@@ -34,7 +35,7 @@ def create_employee(request):
     else:
         form = EmployeeForm()
     return render(request, 'index2.html', {'form': form})
-
+'''
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
@@ -95,6 +96,7 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         super().perform_create(serializer)
+        cache_key = generate_cache_key('employee_list')
         cache.clear()
 
     def perform_update(self, serializer):
@@ -104,6 +106,95 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
         cache.clear()     
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsAuthenticated, CanViewDepartment, CanEditDepartment, CanDeleteDepartment]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('department_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data, timeout=3600)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('department_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data, timeout=3600)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('department_retrieve', instance.pk)
+        set_cache_data(cache_key, DepartmentSerializer(instance).data, timeout=3600)
+        cache_key_list = generate_cache_key('department_list')
+        cache.delete(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('department_retrieve', instance.pk)
+        set_cache_data(cache_key, DepartmentSerializer(instance).data, timeout=3600)
+        cache_key_list = generate_cache_key('department_list')
+        cache.delete(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('department_retrieve', instance.pk)
+        cache.delete(cache_key)
+        cache_key_list = generate_cache_key('department_list')
+        cache.delete(cache_key_list)
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated, CanViewEmployee, CanEditEmployee, CanDeleteEmployee]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('employee_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data, timeout=3600)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('employee_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data, timeout=3600)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('employee_retrieve', instance.pk)
+        set_cache_data(cache_key, EmployeeSerializer(instance).data, timeout=3600)
+        cache_key_list = generate_cache_key('employee_list')
+        cache.delete(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('employee_retrieve', instance.pk)
+        set_cache_data(cache_key, EmployeeSerializer(instance).data, timeout=3600)
+        cache_key_list = generate_cache_key('employee_list')
+        cache.delete(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('employee_retrieve', instance.pk)
+        cache.delete(cache_key)
+        cache_key_list = generate_cache_key('employee_list')
+        cache.delete(cache_key_list)
 
 class DeptEmpViewSet(viewsets.ModelViewSet):
     queryset = DeptEmp.objects.all()
@@ -244,3 +335,268 @@ class TitleViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
         cache.clear()
+'''
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [IsAuthenticated, CanViewDepartment, CanEditDepartment, CanDeleteDepartment]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('department_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('department_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('department_retrieve', instance.pk)
+        set_cache_data(cache_key, DepartmentSerializer(instance).data)
+        cache_key_list = generate_cache_key('department_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('department_retrieve', instance.pk)
+        set_cache_data(cache_key, DepartmentSerializer(instance).data)
+        cache_key_list = generate_cache_key('department_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('department_retrieve', instance.pk)
+        delete_cache_key(cache_key)
+        cache_key_list = generate_cache_key('department_list')
+        delete_cache_key(cache_key_list)
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated, CanViewEmployee, CanEditEmployee, CanDeleteEmployee]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('employee_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('employee_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('employee_retrieve', instance.pk)
+        set_cache_data(cache_key, EmployeeSerializer(instance).data)
+        cache_key_list = generate_cache_key('employee_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('employee_retrieve', instance.pk)
+        set_cache_data(cache_key, EmployeeSerializer(instance).data)
+        cache_key_list = generate_cache_key('employee_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('employee_retrieve', instance.pk)
+        delete_cache_key(cache_key)
+        cache_key_list = generate_cache_key('employee_list')
+        delete_cache_key(cache_key_list)
+
+class DeptEmpViewSet(viewsets.ModelViewSet):
+    queryset = DeptEmp.objects.all()
+    serializer_class = DeptEmpSerializer
+    permission_classes = [IsAuthenticated, CanViewDeptEmp, CanEditDeptEmp, CanDeleteDeptEmp]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('deptemp_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('deptemp_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('deptemp_retrieve', instance.pk)
+        set_cache_data(cache_key, DeptEmpSerializer(instance).data)
+        cache_key_list = generate_cache_key('deptemp_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('deptemp_retrieve', instance.pk)
+        set_cache_data(cache_key, DeptEmpSerializer(instance).data)
+        cache_key_list = generate_cache_key('deptemp_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('deptemp_retrieve', instance.pk)
+        delete_cache_key(cache_key)
+        cache_key_list = generate_cache_key('deptemp_list')
+        delete_cache_key(cache_key_list)
+
+class DeptManagerViewSet(viewsets.ModelViewSet):
+    queryset = DeptManager.objects.all()
+    serializer_class = DeptManagerSerializer
+    permission_classes = [IsAuthenticated, CanViewDeptManager, CanEditDeptManager, CanDeleteDeptManager]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('deptmanager_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('deptmanager_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('deptmanager_retrieve', instance.pk)
+        set_cache_data(cache_key, DeptManagerSerializer(instance).data)
+        cache_key_list = generate_cache_key('deptmanager_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('deptmanager_retrieve', instance.pk)
+        set_cache_data(cache_key, DeptManagerSerializer(instance).data)
+        cache_key_list = generate_cache_key('deptmanager_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('deptmanager_retrieve', instance.pk)
+        delete_cache_key(cache_key)
+        cache_key_list = generate_cache_key('deptmanager_list')
+        delete_cache_key(cache_key_list)
+
+class SalaryViewSet(viewsets.ModelViewSet):
+    queryset = Salary.objects.all()
+    serializer_class = SalarySerializer
+    permission_classes = [IsAuthenticated, CanViewSalary, CanEditSalary, CanDeleteSalary]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('salary_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('salary_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('salary_retrieve', instance.pk)
+        set_cache_data(cache_key, SalarySerializer(instance).data)
+        cache_key_list = generate_cache_key('salary_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('salary_retrieve', instance.pk)
+        set_cache_data(cache_key, SalarySerializer(instance).data)
+        cache_key_list = generate_cache_key('salary_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('salary_retrieve', instance.pk)
+        delete_cache_key(cache_key)
+        cache_key_list = generate_cache_key('salary_list')
+        delete_cache_key(cache_key_list)
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = [IsAuthenticated, CanViewTitle, CanEditTitle, CanDeleteTitle]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('title_list')
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().list(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def retrieve(self, request, *args, **kwargs):
+        cache_key = generate_cache_key('title_retrieve', *args, **kwargs)
+        data = get_cache_data(cache_key)
+        if data:
+            return Response(data)
+        response = super().retrieve(request, *args, **kwargs)
+        set_cache_data(cache_key, response.data)
+        return response
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('title_retrieve', instance.pk)
+        set_cache_data(cache_key, TitleSerializer(instance).data)
+        cache_key_list = generate_cache_key('title_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache_key = generate_cache_key('title_retrieve', instance.pk)
+        set_cache_data(cache_key, TitleSerializer(instance).data)
+        cache_key_list = generate_cache_key('title_list')
+        delete_cache_key(cache_key_list)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache_key = generate_cache_key('title_retrieve', instance.pk)
+        delete_cache_key(cache_key)
+        cache_key_list = generate_cache_key('title_list')
+        delete_cache_key(cache_key_list)
